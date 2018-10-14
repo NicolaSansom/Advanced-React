d3d492430dab9d94f9000ad52b94c7a98202749c
Nicola Sansom <nicolasansom1994@gmail.com>
2018-10-14 23:46:32 +0100

    Add a query and mutation on the public facing server which updates prisma

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,10 @@
-const mutations = {};
+// The resolvers actually reach out to the prisma database and get, add, change or delete things
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO Check if they are logged in
+    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    return item;
+  }
+};
 
 module.exports = mutations;
