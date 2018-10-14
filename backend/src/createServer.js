77687714f42a3cf21e48e1f0d5eea14cc9d61815
Nicola Sansom <nicolasansom1994@gmail.com>
2018-10-14 22:53:15 +0100

    Set up are graphql server that the frontend it will interface with

---
+++ backend/src/createServer.js
@@ -0,0 +1,24 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Mutation = require('./resolvers/Mutation');
+const Query = require('./resolvers/Query');
+const db = require('./db');
+
+// Create the GraphQl Yoga server
+
+function createServer() {
+  return new GraphQLServer({
+    // the schema files cannot be empty
+    typeDefs: 'src/schema.graphql',
+    // for the server to run it requires a mutation & query within the schema file!
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({...req, db}),
+  });
+}
+
+module.exports = createServer;
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,23 @@
-// let's go!
+// Start up are node server
+
+// Get env variables
+require("dotenv").config({ path: "variables.env" });
+const createServer = require("./createServer");
+const db = require("./db");
+
+const server = createServer();
+
+// TODO Use express middleware to handle cookies (JWT)
+// TODO Use express middleware to populate the current user
+
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: process.env.FRONTEND_URL
+    },
+  },
+  details => {
+    console.log(`Server is now running on port http:/localhost${details.port}`);
+  }
+);
