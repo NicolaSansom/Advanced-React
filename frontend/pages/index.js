82b0555cab288bcd4bb4dfdb98225a37a109af24
Nicola Sansom <nicolasansom1994@gmail.com>
2018-10-14 02:10:03 +0100

    Add header and meta information

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,12 +1,8 @@
-import Link from 'next/link';
 import React, { Fragment } from 'react';
 
 const Home = () => (
   <Fragment>
     <p>Home</p>
-    <Link href="/sell">
-      <a>Sell</a>
-    </Link>
   </Fragment>
 );
 
--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,12 +1,8 @@
 import React, { Fragment } from 'react';
-import Link from 'next/link';
 
 const Sell = () => (
   <Fragment>
     <p>Sell</p>
-    <Link href="/">
-      <a>Home</a>
-    </Link>
   </Fragment>
 );
 
