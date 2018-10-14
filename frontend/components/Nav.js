0c4a076d5dba432373c9123b73574378aa9822b9
Nicola Sansom <nicolasansom1994@gmail.com>
2018-10-14 21:28:03 +0100

    Complete styled components & routing module

--- frontend/components/Nav.js
+++ frontend/components/Nav.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
 
 const Nav = () => (
-  <nav>
+  <NavStyles>
+    <Link href="/items">
+      <a>Items</a>
+    </Link>
     <Link href="/sell">
       <a>Sell</a>
-    </Link>{' '}
-    <Link href="/">
-      <a>Home</a>
     </Link>
-  </nav>
+    <Link href="/signup">
+      <a>Sign up</a>
+    </Link>
+    <Link href="/orders">
+      <a>Orders</a>
+    </Link>
+    <Link href="/me">
+      <a>Account</a>
+    </Link>
+  </NavStyles>
 );
 
 export default Nav;
