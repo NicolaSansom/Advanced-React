1030fde05d3ecb27099508c2d9ed4f635d0156d9
Nicola Sansom <nicolasansom1994@gmail.com>
2018-10-17 23:21:02 +0100

    Add update items resolvers

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -4,7 +4,18 @@ const mutations = {
     // TODO Check if they are logged in
     const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
     return item;
-  }
+  },
+  updateItem(parent, args, ctx, info) {
+    // first take a copy of the update
+    const update = { ...args };
+    // remove id
+    delete updates.id;
+    // run the update method
+    return ctx.db.mutation.updateItem({
+      data: { ...update },
+      where: { id: args.id },
+    }, info);
+  },
 };
 
 module.exports = mutations;
