cf954abdac0269ca1ef5ae1f239cb785625c254b
Nicola Sansom <nicolasansom1994@gmail.com>
2018-10-17 22:31:42 +0100

    Add upload image function

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -34,6 +34,21 @@ class CreateItem extends Component {
     this.setState({ [name]: val });
   };
 
+  uploadFile = async e => {
+    const { files } = e.target;
+    const data = new FormData();
+    data.append('file', files[0]);
+    data.append('upload_preset', 'sickFits');
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dwsfmmg8c/image/upload', { method: 'POST', body: data });
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url,
+    });
+  };
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -55,6 +70,11 @@ class CreateItem extends Component {
           >
             <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                File
+                <input type="file" id="file" name="file" placeholder="File" onChange={this.uploadFile} />
+                {this.state.image && <img width="200" src={this.state.image} alt="Upload preview" />}
+              </label>
               <label htmlFor="title">
                 Title
                 <input
