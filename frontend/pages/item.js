3362f8c819b10203135d71a8ea2b40decb44eba3
Nicola Sansom <nicolasansom1994@gmail.com>
2019-03-23 19:58:05 +0000

    Create single item page

---
+++ frontend/pages/item.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import SingleItem from '../components/SingleItem';
+
+const Item = props => (
+  <div>
+    <p>Single Item</p>
+    <SingleItem id={props.query.id} />
+  </div>
+);
+
+export default Item;
