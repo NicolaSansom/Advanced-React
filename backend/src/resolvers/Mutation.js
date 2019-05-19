e9093ddffedfd7a04aa285329cdec1219dc17005
Nicola Sansom <nicolasansom1994@gmail.com>
2019-05-19 17:24:46 +0100

    28 Add sign out button

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -82,6 +82,11 @@ const mutations = {
     // 5. Return the user
     return user;
   },
+
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return { message: 'Goodbye' };
+  },
 };
 
 module.exports = mutations;
