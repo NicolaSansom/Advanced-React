6972003cded17ca359f0a96baaf59f80731a805c
Nicola Sansom <nicolasansom1994@gmail.com>
2019-06-01 20:28:34 +0100

    Update create to check if user logged if so pass user id

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -7,8 +7,24 @@ const { transport, makeANiceEmail } = require('../mail');
 // The resolvers actually reach out to the prisma database and get, add, change or delete things
 const mutations = {
   async createItem(parent, args, ctx, info) {
-    // TODO Check if they are logged in
-    const item = await ctx.db.mutation.createItem({ data: { ...args } }, info);
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in to create an item');
+    }
+
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          // this is how we create a relationship between the Item and the User
+          user: {
+            connect: {
+              id: ctx.request.userId,
+            },
+          },
+          ...args,
+        },
+      },
+      info
+    );
     return item;
   },
   updateItem(parent, args, ctx, info) {
