c5c09df81ae8322ab01a8ed60bf0c71491773124
Nicola Sansom <nicolasansom1994@gmail.com>
2020-04-18 21:49:39 +0100

    42 Delete cart item

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -243,6 +243,22 @@ const mutations = {
       info
     );
   },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. Find the cart item
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: { id: args.id },
+      },
+      `{ id, user { id }}`
+    );
+    if (!cartItem) throw new Error('No cart item found');
+    // 2. Make sure they own the cart item
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('No your item to remove');
+    }
+    // 3. Delete the cart item
+    return ctx.db.mutation.deleteCartItem({ where: { id: args.id } }, info);
+  },
 };
 
 module.exports = mutations;
