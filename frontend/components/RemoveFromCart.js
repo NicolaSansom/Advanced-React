56e7cd044d0c5f778f558a537fe14638352b3e4a
Nicola Sansom <nicolasansom1994@gmail.com>
2020-04-22 17:35:36 +0100

    43 Make removing from cart more visable

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 import gql from 'graphql-tag';
 import { Mutation } from 'react-apollo';
+import { CURRENT_USER_QUERY } from './User';
 
 const REMOVE_FROM_CART_MUTATION = gql`
   mutation removeFromCart($id: ID!) {
@@ -21,8 +22,24 @@ const BigButton = styled.button`
   }
 `;
 
+// this gets called as soon as we get a respoinse back from the server after a mutation has been performed
+const update = (cache, payload) => {
+  //  first read the cache
+  const data = cache.readQuery({ query: CURRENT_USER_QUERY });
+  //  remove that item from the cart
+  const cartItemId = payload.data.removeFromCart.id;
+  data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+  // Write it back to the cache
+  cache.writeQuery({ query: CURRENT_USER_QUERY, data });
+};
+
 const RemoveFromCart = ({ id }) => (
-  <Mutation mutation={REMOVE_FROM_CART_MUTATION} variables={{ id }}>
+  <Mutation
+    mutation={REMOVE_FROM_CART_MUTATION}
+    variables={{ id }}
+    update={update}
+    optimisticResponse={{ __typename: 'Mutation', removeFromCart: { __typeName: 'CartItem', id } }}
+  >
     {(removeFromCart, { error, loading }) => (
       <BigButton
         disabled={loading}
